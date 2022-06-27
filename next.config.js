0a38b9235583f5469c3ef3e5f10263f6c323550c
ZeusCoder <zeusselalusalah@gmail.com>
2022-06-27 17:04:04 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,9 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
   },
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000/api/auth",
+    NEXT_PUBLIC_SECRET:
+      process.env.NEXT_PUBLIC_SECRET || "69f8fd4d54342b7ee3b0fcdf6def434c",
   },
 };
 module.exports = nextConfig;
