75f2f7166f39c03734592ab917bdf3fae8da63d4
ZeusCoder <zeusselalusalah@gmail.com>
2022-07-20 08:01:29 +0700

    update

--- components/auth/SignInButton.js
+++ components/auth/SignInButton.js
@@ -10,7 +10,7 @@ const SignInButton = ({ userToggle }) => {
       }}
       className="block w-full text-sm py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
     >
-      Sign In
+      Login
     </div>
   );
 };
